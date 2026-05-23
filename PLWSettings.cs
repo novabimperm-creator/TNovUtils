12d728e4165055ad3c5f814fe16c7bbb6384c5dd
novabimperm-creator <novabimperm@gmail.com>
2026-05-23 21:32:21 +0500

    Update PLWSettings.cs
    Config

--- PLWSettings.cs
+++ PLWSettings.cs
@@ -1,4 +1,4 @@
-﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.Attributes;
 using System.Collections.Generic;
@@ -24,17 +24,45 @@ namespace TNovUtils
         }
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            string TNovClassName = "Закреплятор Уровни Наборы"; DateTime dateTime = DateTime.Now; string TNovVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            //подключение приложения и документа
-            if (RevitAPI.UiApplication == null) { RevitAPI.Initialize(commandData); }
-            UIDocument uidoc = RevitAPI.UiDocument; Document doc = RevitAPI.Document;
-            UIApplication uiApp = RevitAPI.UiApplication; Autodesk.Revit.ApplicationServices.Application rvtApp = uiApp.Application;
-            
-            //проверка подключения, запись в журнал
-            if(ServerUtils.CheckConnection(TNovClassName, TNovVersion)==false) return Result.Failed;
+            #region Исходные
+            DateTime dateTime = DateTime.Now;
+            string TNovVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            string DBCommandName = "Закреплятор Уровни Наборы";
+            //подключение приложения и документа
+            if (RevitAPI.UiApplication == null) { RevitAPI.Initialize(commandData); }
+            UIDocument uidoc = RevitAPI.UiDocument; Document doc = RevitAPI.Document;
+            UIApplication uiApp = RevitAPI.UiApplication; Autodesk.Revit.ApplicationServices.Application rvtApp = uiApp.Application;
+            string docName = doc.Title.ToString(); docName = docName.Replace(",", " ");
+            string userName = rvtApp.Username; userName = userName.Replace(",", "");
+            string docNameUserName = "_" + userName; docName = docName.Replace(docNameUserName, "");
+            docName = docName.Replace(",", "");
+            #endregion
+            
+            TNovConfig config = TNovConfigLoad.LoadConfig(DBCommandName, TNovVersion);
+            
+            #region Настройки логов
+            // создание log - файла
+            Logger.Initialize(DBCommandName, dateTime, TNovVersion);
 
-            // создание log - файла
-            Logger.Initialize(TNovClassName,dateTime,TNovVersion);
+            var viewModel0 = new AppVersionViewModel();
+
+            string jsonpath0 = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "TNovClient/TNovSettings.json");
+            viewModel0 = JsonConvert.DeserializeObject<AppVersionViewModel>(File.ReadAllText(jsonpath0));
+            if (viewModel0.extendedLogs)
+
+            {
+                var qViewModel = new QuestionWindowViewModel();
+                qViewModel.headtxt = "Включены расширенные логи. " +
+                    "Плагин будет работать медленнее, но соберет больше данных. " +
+                    "Выключить расширенные логи для ускорения работы?";
+                var qwpfview = new QuestionWindow280(qViewModel);
+                qViewModel.CloseRequest += (s, e) => qwpfview.Close();
+                bool? qok = qwpfview.ShowDialog();
+                if (qok != null && qok == true) { Logger.TurnOffExtendedLogs(); } else Logger.Log("Расширенные логи вкл", 2);
+            }
+            #endregion
+
+            
             Logger.Log("Старт работы (настройки)", 0);
 
             //Диалог
